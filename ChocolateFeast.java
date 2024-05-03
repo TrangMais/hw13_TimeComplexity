ffd6456f659bda0dada6c3c8edd7e624e6e96873
TrangMais <80335608+TrangMais@users.noreply.github.com>
2024-05-03 11:53:08 +0200

    Add files via upload

--- ChocolateFeast.java
+++ ChocolateFeast.java
@@ -10,55 +10,43 @@ public class ChocolateFeast {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please enter the number of test cases: ");
-        int t = input.nextInt();
-        
-        System.out.println("Please enter the money, cost and exchange of test cases: ");
-        for(int i = 0; i < t; i++){
-            int n = input.nextInt();
-            int c = input.nextInt();
-            int m = input.nextInt();
-            int wrap = theFeast(n, c, m);
-            int wrap2 = theFeastUpgrade(n, c, m);
-            int wrap3 = theFeastBasic(n, c, m);
-            System.out.println(wrap);
-            System.out.println(wrap2);
-            System.out.println(wrap3);
-        } 
+		int t = input.nextInt();
+
+		System.out.println("Please enter the money, cost and exchange of test cases: ");
+		for (int i = 0; i < t; i++) {
+			int n = input.nextInt();
+			int c = input.nextInt();
+			int m = input.nextInt();
+			int wrapBasic = theFeastUpgrade(n, c, m);
+			int wrapUpgrade = theFeastBasic(n, c, m);
+			System.out.println(wrapBasic);
+			System.out.println(wrapUpgrade);
+		}
 	}
+
 	// Time Complexity: O(n)
-	public static int theFeast(int n, int c, int m) {
-		int barCount = (int) Math.floor(n/c);
+	public static int theFeastBasic(int n, int c, int m) {
+		int barCount = n / c;
 		int wrap = barCount;
-		int newBar = 0;
-		while(wrap >= m) {
-			newBar = wrap/m;
-			barCount = barCount + newBar;
-			wrap = newBar + wrap%m;			//loop will stop when wrap = 1; (or wrap < m)
+		int newBarCount = 0;
+
+		while (wrap >= m) {
+			newBarCount = wrap / m;
+			barCount = barCount + newBarCount; 				// barCount = barCount + wrap/m = barCount + barCount/m
+			wrap = (wrap % m) + newBarCount; 				// wrap = (wrap % m) + (wrap / m)
 		}
 		return barCount;
 	}
-	
+
 	// Time Complexity: O(1)
 	public static int theFeastUpgrade(int n, int c, int m) {
-		int barCount = n/c;				
-		
-		if(barCount >= m) {
-			barCount = barCount + (barCount - 1)/(m-1);
+		int barCount = n / c;
+
+		if (barCount >= m) {
+			barCount = barCount + (barCount - 1) / (m - 1); // loop will stop when wrap is 1 or newBarCount is 1
 			return barCount;
 		}
 		return barCount;
 	}
-	
-	public static int theFeastBasic(int n, int c, int m) {
-		int barCount = n/c;
-		int wrap = barCount;
-		int newBarCount = 0;
-		
-		while(wrap >= m) {
-			newBarCount = wrap/m;
-			barCount = barCount + newBarCount;
-			wrap = (wrap % m) + newBarCount;
-		}
-		return barCount;
-	}
+
 }
--- MinimumGap.java
+++ MinimumGap.java
@@ -39,5 +39,5 @@ public class MinimumGap {
 		}
 		return min;
 	}
-
+	
 }
